9b63edfce88fae4451ee8491c0cd60c2ec160c2b
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-07-17 15:09:00 +0200

    return color

--- js/main.js
+++ js/main.js
@@ -2,32 +2,23 @@ var pseudo = document.getElementById('pseudo');
 var ok = document.getElementById('ok');
 var welcome = document.getElementById('welcome');
 var value;
-var color = ["#3498DB", "FF2D00", "#2E0927", "#04756F", "#85DB18", "#94090D", "#31353D"]
 var img = document.getElementsByTagName('img');
 var test = document.getElementById('test');
-var panda = ["img/panda.jpg"];
+var index = 0;
+var color = ["#3498DB", "#FF2D00", "#2E0927", "#04756F", "#85DB18", "#94090D", "#31353D", "#3498DB", "#FF2D00", "#2E0927", "#04756F", "#85DB18", "#94090D", "#31353D"];
 
 //var img = color[Math.floor(Math.random()*color.length)];
 
-ok.onclick=function() {
+ok.onclick = function() {
   value = pseudo.value;
-  welcome.innerHTML= 'Welcome '+ value.toUpperCase() + ", let's play !";
-  pseudo.style.display='none';
-  ok.style.display='none';
+  welcome.innerHTML = 'Welcome ' + value.toUpperCase() + ", let's play !";
+  pseudo.style.display = 'none';
+  ok.style.display = 'none';
 }
 
-
-
-for (var i=0; i<img.length; i++){
-  img[i].addEventListener("click", function changeColor(){
-    img[i].style.backgroundColor="#3498DB";
+for (let i = 0; i < img.length; i++) {
+  img[i].addEventListener("click", function() {
+    img[i].src = "";
+    img[i].style.backgroundColor = color[i];
   });
 }
-
-
-/*img[0].addEventListener("click", changeColor);
-
-function changeColor(){
-  img[0].style.visibility="hidden";
-  //img[0,1,2,3].style.color="purple";
-}*/
