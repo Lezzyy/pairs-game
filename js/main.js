0b963528ba9c85c603a0f6b2e52499c77bf69981
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-07-19 15:54:38 +0200

    add function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// VARIABLES
+
 var pseudo = document.getElementById('pseudo');
 var ok = document.getElementById('ok');
 var welcome = document.getElementById('welcome');
@@ -7,14 +9,11 @@ var test = document.getElementById('test');
 var index = 0;
 var color = ["#3498DB", "#FF2D00", "#2E0927", "#04756F", "#85DB18", "#94090D", "#31353D", "#3498DB", "#FF2D00", "#2E0927", "#04756F", "#85DB18", "#94090D", "#31353D"];
 var place;
-shuffle();
+var init = 0;
+var tabResult = [];
 
-ok.onclick = function() {
-  value = pseudo.value;
-  welcome.innerHTML = 'Welcome ' + value.toUpperCase() + ", let's play !";
-  pseudo.style.display = 'none';
-  ok.style.display = 'none';
-}
+
+// FUNCTIONS
 
 function shuffle() {
   for (place = color.length - 1; place >= 1; place--) {
@@ -25,28 +24,74 @@ function shuffle() {
   }
 }
 
-var init = 0;
-var tabResult = [];
+function setBackgroundColor() {
+  for (let i = 0; i < img.length; i++) {
+    img[i].onclick = function() {
+        img[i].src = "";
+        img[i].style.backgroundColor = color[i];
 
-for (let i = 0; i < img.length; i++) {
-  img[i].addEventListener("click", function() {
-    img[i].src = "";
-    img[i].style.backgroundColor = color[i];
-
-    tabResult.push(color[i]);
-    init++;
-    if (tabResult.length == 2) {
-      if (tabResult[0] == tabResult[1]) {
-        console.log(tabResult);
-        alert("gagné");
-        tabResult = [];
-      } else if (tabResult[0] !== tabResult[1]) {
-        alert('perdu');
-        img[i].src = "img/plage.jpg" ;
-        img[i-1].src = "img/plage.jpg" ;
-        console.log(img[i]);
-        tabResult = [];
+        tabResult.push(color[i]);
+        init++;
       }
     }
-  });
+  }
+
+  function name() {
+    value = pseudo.value;
+    welcome.innerHTML = 'Welcome ' + value.toUpperCase() + ", let's play !";
+    pseudo.style.display = 'none';
+    ok.style.display = 'none';
+  }
+
+function playGame() {
+  shuffle();
+  setBackgroundColor();
 }
+
+playGame();
+
+ok.onclick = name;
+
+
+// if (tabResult.length == 2);
+
+function compare() {
+    if (tabResult[0] == tabResult[1]) {
+      alert("gagné");
+      tabResult = [];
+    } else if (tabResult[0] !== tabResult[1]) {
+      alert('perdu');
+      img[i].src = "img/plage.jpg";
+      // img[i].style.backgroundImage = img;
+      tabResult = [];
+    }
+  }
+
+
+
+
+
+
+
+// for (let i = 0; i < img.length; i++) {
+//   img[i].addEventListener("click", function() {
+//     img[i].src = "";
+//     img[i].style.backgroundColor = color[i];
+//
+//     tabResult.push(color[i]);
+//     init++;
+//     if (tabResult.length == 2) {
+//       if (tabResult[0] == tabResult[1]) {
+//         console.log(tabResult);
+//         alert("gagné");
+//         tabResult = [];
+//       } else if (tabResult[0] !== tabResult[1]) {
+//         alert('perdu');
+//         img[i].src = "img/plage.jpg" ;
+//         img[i-1].src = "img/plage.jpg" ;
+//         console.log(img[i]);
+//         tabResult = [];
+//       }
+//     }
+//   });
+// }
