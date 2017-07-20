82318dabd39a82658518e9e18a2e6dc5127a14ae
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-07-20 10:01:31 +0200

    return card ok

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
 var pseudo = document.getElementById('pseudo');
 var ok = document.getElementById('ok');
 var welcome = document.getElementById('welcome');
+var score = document.getElementById('score');
+var clics = document.getElementById('clics');
 var value;
 var img = document.getElementsByTagName('img');
 var test = document.getElementById('test');
-var index = 0;
 var color = ["#3498DB", "#FF2D00", "#2E0927", "#04756F", "#85DB18", "#94090D", "#31353D", "#3498DB", "#FF2D00", "#2E0927", "#04756F", "#85DB18", "#94090D", "#31353D"];
 var place;
 var init = 0;
@@ -24,48 +25,57 @@ function shuffle() {
   }
 }
 
+
 function setBackgroundColor() {
   for (let i = 0; i < img.length; i++) {
     img[i].onclick = function() {
-        img[i].src = "";
-        img[i].style.backgroundColor = color[i];
+      img[i].src = "";
+      img[i].style.backgroundColor = color[i];
 
-        tabResult.push(color[i]);
-        init++;
+      tabResult.push(i);
+      init++;
+
+      if (tabResult.length == 2) {
+        if (color[tabResult[0]] == color[tabResult[1]]) {
+          console.log(color[tabResult[0]]);
+          score.innerHTML="YOU WON !";
+          // clics.innerHTML = (tabResult.length);
+          tabResult = [];
+        } else if (tabResult[0] !== tabResult[1]) {
+          alert('perdu');
+          // score.innerHTML="YOU LOSE !";
+          img[i].src = "img/plage.jpg";
+          img[tabResult[0]].src = "img/plage.jpg";
+          console.log(img[tabResult[0]]);
+          // clics.innerHTML = (tabResult.length);
+
+          tabResult = [];
+        }
       }
     }
   }
+}
 
-  function name() {
-    value = pseudo.value;
-    welcome.innerHTML = 'Welcome ' + value.toUpperCase() + ", let's play !";
-    pseudo.style.display = 'none';
-    ok.style.display = 'none';
-  }
+function name() {
+  value = pseudo.value;
+  welcome.innerHTML = 'Welcome ' + value.toUpperCase() + ", let's play !";
+  pseudo.style.display = 'none';
+  ok.style.display = 'none';
+}
 
 function playGame() {
   shuffle();
   setBackgroundColor();
+
 }
 
 playGame();
 
+
+
 ok.onclick = name;
 
 
-// if (tabResult.length == 2);
-
-function compare() {
-    if (tabResult[0] == tabResult[1]) {
-      alert("gagné");
-      tabResult = [];
-    } else if (tabResult[0] !== tabResult[1]) {
-      alert('perdu');
-      img[i].src = "img/plage.jpg";
-      // img[i].style.backgroundImage = img;
-      tabResult = [];
-    }
-  }
 
 
 
